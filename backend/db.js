40b57a4a68f20d798ccb12d30ab06db79bae823f
Tushar Jindal <112417904+TusharIndia@users.noreply.github.com>
2024-08-28 00:01:07 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const mongoose = require('mongoose')
-const mongouri = process.env.MONGODB_URI
+// const mongouri = process.env.MONGODB_URI
+const mongouri = "mongodb+srv://tj61676:61676@cluster0.n7ex1vf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 const connectToMongo = async () => {
     await mongoose.connect(mongouri)
